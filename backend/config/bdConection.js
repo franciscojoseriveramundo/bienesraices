f53e1e3f9dcb247d0df9dbad53c7d1e001ac35c3
Francisco <francisco.rivera@servitron.net>
2022-03-25 02:23:08 -0600

    Separación del front end con el API Rest.

--- backend/config/bdConection.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('bkvr6afh90lntut7zlay', 'uccyyf00d6p1cs9k', 'bAiGtlCuannbB7tZDT6T', {
-  host: 'bkvr6afh90lntut7zlay-mysql.services.clever-cloud.com',
-  // una de estas opciones dependiendo el gestor de la base
-  dialect: 'mysql',
-})
-
-module.exports = sequelize;
--- backend/config/env.sh
+++
@@ -1,4 +0,0 @@
-export HOST = 'bkvr6afh90lntut7zlay-mysql.services.clever-cloud.com'
-export DATABASE = 'bkvr6afh90lntut7zlay'
-export PASSWORD = 'bAiGtlCuannbB7tZDT6T'
-export USERDB = 'uccyyf00d6p1cs9k'
